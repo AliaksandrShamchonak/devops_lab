02a0ad2267993108cea6b7671485eb44b3df1b3f
AliaksandrShamchonak <51114482+AliaksandrShamchonak@users.noreply.github.com>
2019-06-22 16:42:22 +0300

    Update sysdata.py

--- overallsystemdata/sysdata.py
+++ overallsystemdata/sysdata.py
@@ -31,13 +31,9 @@ def printres():
             "Network information": netw
         }, indent=4)
     else:
-        sprintres = "SNAPSHOT " + str(count) + ": " +\
-                timenow + ":" +\
-                " CPU load %: " + cpu +\
-                " Memory usage MB: " + memu +\
-                " Virtual memory usage MB: " + memv +\
-                " IO information " + ioc +\
-                " Network information " + netw + "\n"
+        sprintres = "SNAPSHOT " + str(count) + ": " + timenow + ":" + " CPU load %: " + cpu +\
+                " Memory usage MB: " + memu + " Virtual memory usage MB: " + memv +\
+                " IO information " + ioc + " Network information " + netw + "\n"
     print(sprintres)
     return sprintres
 
