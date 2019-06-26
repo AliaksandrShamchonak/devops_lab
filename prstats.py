081f9fa5715e72f4053a449d8563cf88ffb4d3e2
AliaksandrShamchonak <51114482+AliaksandrShamchonak@users.noreply.github.com>
2019-06-26 19:26:05 +0300

    Update prstats.py

--- prstats.py
+++ prstats.py
@@ -21,6 +21,8 @@ parser.add_argument("-pu", "--pull", action='store_true', default='pulls',
                     help="Show number of pull requests and create date")
 parser.add_argument("-pc", "--pullclose", action='store_true',
                     help="Show pull requests is closed")
+parser.add_argument("-wd", "--weekday", action='store_true',
+                    help="Day of week pull request created")
 parser.add_argument("--uname", action='store_true',
                     help="User name for authorization github")
 parser.add_argument("--utoken", action='store_true',
