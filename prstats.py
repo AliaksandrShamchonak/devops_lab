a2bcc24c36b740cfbc4ecf300c8410201f685997
AliaksandrShamchonak <51114482+AliaksandrShamchonak@users.noreply.github.com>
2019-06-26 20:06:18 +0300

    Update prstats.py

--- prstats.py
+++ prstats.py
@@ -1,18 +1,16 @@
 import argparse
 import requests
 import calendar
-import datetime
+from datetime import datetime
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-v', '--version', action='version', version='version 1.0',
                     help='Prints the version of program')
 parser.add_argument("-o", "--owner", help="Owner of repository",
-                    action='store_true', default='alenaPy')
+                    default='alenaPy')
 parser.add_argument("-r", "--repo", help="Name of repository",
-                    action='store_true', default='devops_lab')
-
+                    default='devops_lab')
 parser.add_argument('-a', '--all', help='Show all parameters', action='store_true')
-
 parser.add_argument('-cm', '--commit', action='store_true', default='commits',
                     help='Show the number of commits, date and names for repository')
 parser.add_argument('-br', '--branches', action='store_true', default='branches',
@@ -23,9 +21,9 @@ parser.add_argument("-pc", "--pullclose", action='store_true',
                     help="Show pull requests is closed")
 parser.add_argument("-wd", "--weekday", action='store_true',
                     help="Day of week pull request created")
-parser.add_argument("--uname", action='store_true',
+parser.add_argument("-un", "--uname", type=str, required=True,
                     help="User name for authorization github")
-parser.add_argument("--utoken", action='store_true',
+parser.add_argument("-ut", "--utoken", type=str, required=True,
                     help="User token for authorization github")
 
 args = parser.parse_args()
@@ -39,7 +37,7 @@ url = ''
 
 def urlin(urlowner, urlrepo, urloptions):
     global url
-    url = "https://api.github.com/repos/%s/%s/%s" % (urlowner, urlrepo, urloptions)
+    url = "https://api.github.com/repos/%s/%s/%s" %(urlowner, urlrepo, urloptions)
 
 
 if args.commit or args.all:
@@ -47,7 +45,7 @@ if args.commit or args.all:
     urlin(urlowner, urlrepo, urloptions)
     r = requests.get(url, auth=(username, token))
     lenstr = len(r.json())
-    print("\nYou have %d commits:" % lenstr)
+    print("\nYou have %d commits:" %lenstr)
     for i in range(lenstr):
         idate = r.json()[i]['commit']['author']['date']
         imessage = r.json()[i]['commit']['message']
@@ -65,7 +63,7 @@ if args.branches or args.all:
         print(iname + " - Protected: " + iprotected)
 
 if args.pull or args.all:
-    urloptions = 'pulls'
+    urloptions = 'pulls?page=1&per_page=100&state=all'
     urlin(urlowner, urlrepo, urloptions)
     r = requests.get(url, auth=(username, token))
     lenstr = len(r.json())
@@ -73,12 +71,13 @@ if args.pull or args.all:
     for i in range(lenstr):
         ititle = r.json()[i]['title']
         icreatedat = r.json()[i]['created_at']
-        print(icreatedat + " - " + ititle)
+        print(icreatedat + " - " + ititle, end='')
 
         if args.pullclose or args.all:
             iclosedate = r.json()[i]['closed_at']
-            print("Pull closed: " + str(iclosedate))
+            print(" - Pull closed: " + str(iclosedate), end='')
 
         if args.weekday or args.all:
-            iweekday = datetime.datetime.strptime(icreatedat, "%Y-%m-%dT%H:%M:%SZ")
-            print("Created day of week:", calendar.day_name[datetime.datetime.weekday(iweekday)])
+            iweekday = datetime.strptime(icreatedat, "%Y-%m-%dT%H:%M:%SZ")
+            print(" - Created day of week:", calendar.day_name[datetime.weekday(iweekday)], end='')
+        print()
